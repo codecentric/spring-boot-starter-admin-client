661f4a422ed1dc7593cad107df3cb7ac84455a6e
Thomas Bosch <tbosch@gmx.de>
2014-06-17 14:02:53 +0200

    check

--- src/main/java/de/codecentric/boot/admin/services/SpringBootAdminRegistrator.java
+++ src/main/java/de/codecentric/boot/admin/services/SpringBootAdminRegistrator.java
@@ -6,23 +6,33 @@ import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
-import org.springframework.beans.factory.annotation.Value;
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.util.Assert;
 import org.springframework.web.client.RestTemplate;
 
 import de.codecentric.boot.admin.model.Application;
 
 public class SpringBootAdminRegistrator {
 
-	@Value("${info.id}")
-	private String id;
+	@Autowired
+	private Environment env;
 
-	@Value("${server.port}")
-	private Integer port;
+	@PostConstruct
+	public void check() {
+		Assert.notNull(env.getProperty("server.port"), "The server port of the application is mandatory");
+		Assert.notNull(env.getProperty("info.id"), "The id of the application is mandatory");
+		Assert.notNull(env.getProperty("info.version"), "The version of the application is mandatory");
+	}
 
 	@Scheduled(cron = "0 0/1 * * * *")
 	public void minutly() throws UnknownHostException, MalformedURLException {
+		String id = env.getProperty("info.id");
+		int port = env.getProperty("server.port", Integer.class);
 		RestTemplate template = new RestTemplate();
 		template.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 		ApplicationList list = template.getForObject("http://localhost:8080/api/applications", ApplicationList.class);
@@ -40,6 +50,7 @@ public class SpringBootAdminRegistrator {
 	}
 
 	private static class ApplicationList extends ArrayList<Application> {
+		private static final long serialVersionUID = 1L;
 	}
 
 }
