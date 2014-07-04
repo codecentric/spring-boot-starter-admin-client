ba6cc1e0aef33329664ea87aff8c5da794aec737
Thomas Bosch <tbosch@gmx.de>
2014-07-04 12:19:08 +0200

    RC4

--- dev/install-to-private-maven-repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Muss im root-Verzeichnis des Projekts ausgeführt werden
-
-LIB=spring-boot-starter-admin-client-1.0.0.RC3
-REPO=/home/thomas/Entwicklung/git/maven-repo/
-
-# Main Sources
-mvn install:install-file -DlocalRepositoryPath=$REPO -DcreateChecksum=true -Dfile=target/$LIB.jar -DpomFile=pom.xml -Dsources=target/$LIB-sources.jar -Djavadoc=target/$LIB-javadoc.jar
